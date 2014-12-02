4279c21d6ea61d6f107a6233b3ea47e1563d5b23
kawinpong <peak_henry@hotmail.com>
2014-12-02 15:26:25 +0700

    Problem1

---
+++ spec/lib/Problem1_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+require "problem1"
+
+
+describe "lovemac" do
+  it "Runs correctly" do
+    expect(lovemac(15)).to eq("HateWindows")
+    expect(lovemac(6)).to eq("Love")
+    expect(lovemac(5)).to eq("Mac")
+    expect(lovemac(10)).to eq("Mac")
+    expect(lovemac(13)).to eq(13)
+    expect(lovemac(14)).to eq(14)
+
+  end
+end
